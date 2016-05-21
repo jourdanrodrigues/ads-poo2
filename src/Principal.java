86275c15192846d7124a8d4bdb0a42780d006e69
Jourdan Rodrigues <thiagojourdan@gmail.com>
2016-05-21 09:15:59 -0300

    Initial commit extension

---
+++ src/Principal.java
@@ -0,0 +1,15 @@
+
+import visao.FormPrincipal;
+
+
+public class Principal {
+
+    
+    public static void main(String[] args) {
+       
+        FormPrincipal fp = new FormPrincipal();
+        fp.setVisible(true);
+        
+    }
+    
+}
